92467e364d9828d00c5948b5d20531c89567eea8
Roland Franssen <franssen.roland@gmail.com>
2023-10-02 11:37:55 +0200

    cfg

---
+++ .php-cs-fixer.php
@@ -0,0 +1,36 @@
+<?php
+
+// @see https://mlocati.github.io/php-cs-fixer-configurator/
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        '@PHP81Migration' => true,
+        '@PHP80Migration:risky' => true,
+        '@PHPUnit84Migration:risky' => true,
+        'header_comment' => ['header' => ''],
+        'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
+        'php_unit_test_class_requires_covers' => false,
+        'no_superfluous_phpdoc_tags' => ['remove_inheritdoc' => true],
+        'global_namespace_import' => ['import_classes' => false, 'import_constants' => false, 'import_functions' => false],
+        'nullable_type_declaration_for_default_null_value' => true,
+    ])
+    ->setCacheFile(__DIR__.'/var/phpcsfixer-cache')
+    ->setFinder(
+        Finder::create()
+            ->in([
+                __DIR__.'/{src,tests}',
+            ])
+            ->append([
+                __FILE__,
+                __DIR__.'/rector.php',
+            ])
+    )
+;
---
+++ rector.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
+    $rectorConfig->cacheDirectory(__DIR__.'/var/rector-cache');
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses(false);
+
+    // https://github.com/rectorphp/rector/blob/main/docs/rector_rules_overview.md
+    // https://github.com/rectorphp/rector-symfony/blob/main/docs/rector_rules_overview.md
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::CODE_QUALITY,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+        SetList::PRIVATIZATION,
+        SetList::STRICT_BOOLEANS,
+        SetList::TYPE_DECLARATION,
+        SymfonyLevelSetList::UP_TO_SYMFONY_63,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+    ]);
+};
