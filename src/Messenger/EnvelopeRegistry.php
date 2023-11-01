b94fbd6ec5032c09151cbf006b48ff9723a3dd69
Roland Franssen <franssen.roland@gmail.com>
2023-11-01 12:00:50 +0100

    get-counts

--- src/Messenger/EnvelopeRegistry.php
+++ src/Messenger/EnvelopeRegistry.php
@@ -26,6 +26,22 @@ final class EnvelopeRegistry
         return $this->counter->delta($sender.':'.$envelope->getMessage()::class, $count);
     }
 
+    /**
+     * @return array<string, array<string, int>>
+     */
+    public function getCounts(): array
+    {
+        $counts = [];
+
+        foreach ($this->transportOptions as $sender => $messages) {
+            foreach ($messages as $message => $_) {
+                $counts[$sender][$message] = $this->counter->get($sender.':'.$message);
+            }
+        }
+
+        return $counts;
+    }
+
     public function getSenderAlias(Envelope $envelope): string
     {
         return $envelope->last(SentStamp::class)?->getSenderAlias() ?? $envelope->last(ReceivedStamp::class)?->getTransportName() ?? 'sync';
