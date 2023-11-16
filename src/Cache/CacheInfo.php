9b1d74a310ff0e0f65deecebd77d9db4beafc5f8
Roland Franssen <franssen.roland@gmail.com>
2023-11-16 12:30:49 +0100

    CacheInfo::toItem

--- src/Cache/CacheInfo.php
+++ src/Cache/CacheInfo.php
@@ -4,12 +4,36 @@ declare(strict_types=1);
 
 namespace Etrias\PhpToolkit\Cache;
 
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
 final class CacheInfo
 {
     public function __construct(
         public readonly string $key,
-        public readonly int|float|null $ttl = null,
+        public readonly null|\DateInterval|\DateTimeInterface|int $ttl = null,
         public readonly array $tags = [],
-    ) {
+    ) {}
+
+    public function toItem(CacheItemPoolInterface $cache): CacheItemInterface
+    {
+        $item = $cache->getItem($this->key);
+
+        if ($this->ttl instanceof \DateTimeInterface) {
+            $item->expiresAt($this->ttl);
+        } else {
+            $item->expiresAfter($this->ttl);
+        }
+
+        if ($this->tags) {
+            if (!$item instanceof ItemInterface) {
+                throw new \LogicException('Can only tag cache items of type '.ItemInterface::class);
+            }
+
+            $item->tag($this->tags);
+        }
+
+        return $item;
     }
 }
