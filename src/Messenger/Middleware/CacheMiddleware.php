dc2869fca5d59d300e4e66b84dbb2ab26dca8ed2
Roland Franssen <franssen.roland@gmail.com>
2023-11-16 15:21:33 +0100

    CacheMiddleware

--- src/Messenger/Middleware/CacheMiddleware.php
+++ src/Messenger/Middleware/CacheMiddleware.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Etrias\PhpToolkit\Messenger\Middleware;
 
+use Etrias\PhpToolkit\Cache\CacheInfoProvider;
 use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Component\Messenger\Envelope;
@@ -15,10 +16,26 @@ final class CacheMiddleware implements MiddlewareInterface
     public function __construct(
         #[Target(name: 'messenger.cache')]
         private readonly CacheItemPoolInterface $cache,
+        private readonly CacheInfoProvider $cacheInfoProvider,
     ) {}
 
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
-        return $stack->next()->handle($envelope, $stack);
+        $result = $stack->next()->handle($envelope, $stack);
+
+        if (null === $cacheInfo = $this->cacheInfoProvider->get($envelope->getMessage())) {
+            return $result;
+        }
+
+        $cacheItem = $cacheInfo->toItem($this->cache);
+
+        if ($cacheItem->isHit()) {
+            return $cacheItem->get();
+        }
+
+        $cacheItem->set($result);
+        $this->cache->save($cacheItem);
+
+        return $result;
     }
 }
