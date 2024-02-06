d60061bde2287554bfc2475e9ffd14da61720d0c
etrias <it@etrias.nl>
2024-02-06 16:30:42 +0000

    Apply automatic changes

--- src/Messenger/Transport/NatsTransport.php
+++ src/Messenger/Transport/NatsTransport.php
@@ -137,9 +137,7 @@ final class NatsTransport implements TransportInterface, MessageCountAwareInterf
     /**
      * @see https://github.com/symfony/symfony/issues/52642
      */
-    public function reject(Envelope $envelope): void
-    {
-    }
+    public function reject(Envelope $envelope): void {}
 
     public function send(Envelope $envelope): Envelope
     {
