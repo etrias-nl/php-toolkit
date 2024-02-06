7d4e571b5f34da55257b2b05b96327e74584a9f9
Roland Franssen <franssen.roland@gmail.com>
2024-02-06 14:37:32 +0100

    minor

--- src/Messenger/Middleware/DoctrineTransactionMiddleware.php
+++ src/Messenger/Middleware/DoctrineTransactionMiddleware.php
@@ -7,13 +7,16 @@ namespace Etrias\PhpToolkit\Messenger\Middleware;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Messenger\DoctrineTransactionMiddleware as BaseDoctrineTransactionMiddleware;
 use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\Middleware\StackInterface;
 use Symfony\Component\Messenger\Stamp\ConsumedByWorkerStamp;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 
 /**
  * @internal
  *
  * @see https://github.com/symfony/symfony/issues/51993
+ * @see https://github.com/symfony/symfony/pull/53809
  */
 final class DoctrineTransactionMiddleware extends BaseDoctrineTransactionMiddleware
 {
@@ -23,6 +26,26 @@ final class DoctrineTransactionMiddleware extends BaseDoctrineTransactionMiddlew
             return $stack->next()->handle($envelope, $stack);
         }
 
-        return parent::handleForManager($entityManager, $envelope, $stack);
+        $entityManager->getConnection()->beginTransaction();
+
+        try {
+            $envelope = $stack->next()->handle($envelope, $stack);
+            $entityManager->flush();
+            $entityManager->getConnection()->commit();
+
+            return $envelope;
+        } catch (\Throwable $exception) {
+            if ($entityManager->getConnection()->isTransactionActive()) {
+                $entityManager->getConnection()->rollBack();
+            }
+
+            if ($exception instanceof HandlerFailedException) {
+                // Remove all HandledStamp from the envelope so the retry will execute all handlers again.
+                // When a handler fails, the queries of allegedly successful previous handlers just got rolled back.
+                throw new HandlerFailedException($exception->getEnvelope()->withoutAll(HandledStamp::class), $exception->getWrappedExceptions());
+            }
+
+            throw $exception;
+        }
     }
 }
