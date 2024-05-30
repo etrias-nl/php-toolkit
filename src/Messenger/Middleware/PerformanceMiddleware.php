5880beeb30c90cc0c7eb2876f7fff68459d7d0a4
Roland Franssen <franssen.roland@gmail.com>
2024-05-30 15:06:12 +0200

    bench query

--- src/Messenger/Middleware/PerformanceMiddleware.php
+++ src/Messenger/Middleware/PerformanceMiddleware.php
@@ -5,21 +5,33 @@ declare(strict_types=1);
 namespace Etrias\PhpToolkit\Messenger\Middleware;
 
 use Etrias\PhpToolkit\Performance\Benchmark;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
 use Symfony\Component\Messenger\Stamp\ReceivedStamp;
+use Symfony\Component\Messenger\Transport\Sender\SendersLocatorInterface;
 
 final class PerformanceMiddleware implements MiddlewareInterface
 {
     public function __construct(
         private readonly Benchmark $benchmark,
+        #[Autowire(service: 'messenger.senders_locator')]
+        private readonly SendersLocatorInterface $sendersLocator,
     ) {}
 
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
         if (null === $envelope->last(ReceivedStamp::class)) {
-            return $stack->next()->handle($envelope, $stack);
+            $hasSender = false;
+            foreach ($this->sendersLocator->getSenders($envelope) as $sender) {
+                $hasSender = true;
+                break;
+            }
+
+            if ($hasSender) {
+                return $stack->next()->handle($envelope, $stack);
+            }
         }
 
         return $this->benchmark->run($envelope->getMessage()::class, static fn (): Envelope => $stack->next()->handle($envelope, $stack));
