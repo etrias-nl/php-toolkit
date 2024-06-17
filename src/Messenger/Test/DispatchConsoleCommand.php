70a665dd08944ea0fd545dc04452d14f3f995006
Roland Franssen <franssen.roland@gmail.com>
2024-06-17 12:56:07 +0200

    minor

--- src/Messenger/Test/DispatchConsoleCommand.php
+++ src/Messenger/Test/DispatchConsoleCommand.php
@@ -6,10 +6,12 @@ namespace Etrias\PhpToolkit\Messenger\Test;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Dumper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\TransportNamesStamp;
 
@@ -32,29 +34,45 @@ class DispatchConsoleCommand extends Command
             ->addOption('nest-failure', null, InputOption::VALUE_NONE)
             ->addOption('nest-sync', null, InputOption::VALUE_NONE)
             ->addOption('batch', null, InputOption::VALUE_REQUIRED, '', 1)
+            ->addOption('class', null, InputOption::VALUE_REQUIRED)
             ->addArgument('payload', InputArgument::OPTIONAL)
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+        $dumper = new Dumper($output);
+
         if (null !== $payload = $input->getArgument('payload')) {
             try {
                 $payload = json_decode($payload, true, 512, JSON_THROW_ON_ERROR);
             } catch (\JsonException) {
             }
         }
+        if (null !== ($class = $input->getOption('class')) && !class_exists($class)) {
+            throw new \InvalidArgumentException('Class not found: '.$class);
+        }
 
         for ($i = 1; $i <= (int) $input->getOption('batch'); ++$i) {
             $output->writeln('Dispatching message #'.$i);
-            $message = new DummyMessage(
-                $payload ?? 'Batch message #'.$i,
-                (float) $input->getOption('sleep'),
-                $input->getOption('failure'),
-                $input->getOption('nest'),
-                $input->getOption('nest-failure'),
-                $input->getOption('nest-sync'),
-            );
+            if (null === $class) {
+                $message = new DummyMessage(
+                    $payload ?? 'Batch message #'.$i,
+                    (float) $input->getOption('sleep'),
+                    $input->getOption('failure'),
+                    $input->getOption('nest'),
+                    $input->getOption('nest-failure'),
+                    $input->getOption('nest-sync'),
+                );
+            } else {
+                $message = \is_array($payload) ? new $class(...$payload) : new $class($payload);
+                $io->writeln($dumper($message));
+                if (!$io->confirm('Dispatch?', false)) {
+                    continue;
+                }
+            }
+
             $stamps = $input->getOption('sync') ? [new TransportNamesStamp('sync')] : [];
 
             $this->messageBus->dispatch($message, $stamps);
