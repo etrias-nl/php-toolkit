d7185f7801c69d94a3fc03a415ba654fae2a84e9
Roland Franssen <1047696+ro0NL@users.noreply.github.com>
2024-09-18 12:11:24 +0200

    tweak

--- src/Messenger/Middleware/DoctrineConnectionMiddleware.php
+++ src/Messenger/Middleware/DoctrineConnectionMiddleware.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace Etrias\PhpToolkit\Messenger\Middleware;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\Persistence\ConnectionRegistry;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Etrias\PhpToolkit\Messenger\MessageMap;
+use Etrias\PhpToolkit\Messenger\Stamp\TransactionalStamp;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
 use Symfony\Component\Messenger\Stamp\ConsumedByWorkerStamp;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 
 /**
  * @internal
@@ -18,8 +23,11 @@ use Symfony\Component\Messenger\Stamp\ConsumedByWorkerStamp;
 final class DoctrineConnectionMiddleware implements MiddlewareInterface
 {
     public function __construct(
-        #[Autowire(service: 'doctrine')]
-        private readonly ConnectionRegistry $connectionRegistry,
+        private readonly LoggerInterface $logger,
+        private readonly MessageMap $messageMap,
+        private readonly ManagerRegistry $managerRegistry,
+        private readonly ?string $entityManagerName = null,
+        private readonly int $waitTimeout = 28800,
     ) {}
 
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
@@ -28,13 +36,55 @@ final class DoctrineConnectionMiddleware implements MiddlewareInterface
             return $stack->next()->handle($envelope, $stack);
         }
 
-        try {
-            return $stack->next()->handle($envelope, $stack);
-        } finally {
-            /** @var Connection $connection */
-            foreach ($this->connectionRegistry->getConnections() as $connection) {
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $this->managerRegistry->getManager($this->entityManagerName);
+        $connection = $entityManager->getConnection();
+        $transactional = $this->messageMap->getStamp($envelope, TransactionalStamp::class)?->enabled ?? true;
+
+        if (!$transactional) {
+            $this->setWaitTimeout($connection);
+
+            try {
+                return $stack->next()->handle($envelope, $stack);
+            } finally {
                 $connection->close();
             }
         }
+
+        $connection->beginTransaction();
+
+        $this->setWaitTimeout($connection);
+
+        try {
+            $envelope = $stack->next()->handle($envelope, $stack);
+            $entityManager->flush();
+            $connection->commit();
+
+            return $envelope;
+        } catch (\Throwable $exception) {
+            try {
+                $connection->rollBack();
+            } catch (\Throwable $rollbackException) {
+                $this->logger->error('An error occurred while rolling back the transaction', [
+                    'exception' => $rollbackException,
+                ]);
+            }
+
+            if ($exception instanceof HandlerFailedException) {
+                // Remove all HandledStamp from the envelope so the retry will execute all handlers again.
+                // When a handler fails, the queries of allegedly successful previous handlers just got rolled back.
+                throw new HandlerFailedException($exception->getEnvelope()->withoutAll(HandledStamp::class), $exception->getWrappedExceptions());
+            }
+
+            throw $exception;
+        } finally {
+            $connection->close();
+        }
+    }
+
+    private function setWaitTimeout(Connection $connection): void
+    {
+        $connection->executeQuery('SET session wait_timeout = '.$this->waitTimeout);
+        $connection->executeQuery('SET session interactive_timeout = '.$this->waitTimeout);
     }
 }
