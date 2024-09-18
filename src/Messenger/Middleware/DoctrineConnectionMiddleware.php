c2d237742fecaa74634a501152c870d69a700b26
Roland Franssen <1047696+ro0NL@users.noreply.github.com>
2024-09-18 11:21:04 +0200

    connection-middleware

---
+++ src/Messenger/Middleware/DoctrineConnectionMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Etrias\PhpToolkit\Messenger\Middleware;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\Persistence\ConnectionRegistry;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
+use Symfony\Component\Messenger\Middleware\StackInterface;
+use Symfony\Component\Messenger\Stamp\ConsumedByWorkerStamp;
+
+/**
+ * @internal
+ */
+final class DoctrineConnectionMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly ConnectionRegistry $connectionRegistry,
+    ) {}
+
+    public function handle(Envelope $envelope, StackInterface $stack): Envelope
+    {
+        if (null === $envelope->last(ConsumedByWorkerStamp::class)) {
+            return $stack->next()->handle($envelope, $stack);
+        }
+
+        try {
+            return $stack->next()->handle($envelope, $stack);
+        } finally {
+            /** @var Connection $connection */
+            foreach ($this->connectionRegistry->getConnections() as $connection) {
+                $connection->close();
+            }
+        }
+    }
+}
