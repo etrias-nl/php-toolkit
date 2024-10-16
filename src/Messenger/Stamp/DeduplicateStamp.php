016def2f16d555ce56f5c5e7d0fcffbfd29b1217
Roland Franssen <1047696+ro0NL@users.noreply.github.com>
2024-10-16 10:16:43 +0200

    minor

--- src/Messenger/Stamp/DeduplicateStamp.php
+++ src/Messenger/Stamp/DeduplicateStamp.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Etrias\PhpToolkit\Messenger\Stamp;
 
-use Symfony\Component\Messenger\Stamp\StampInterface;
+use Symfony\Component\Messenger\Stamp\NonSendableStampInterface;
 
 #[\Attribute(\Attribute::TARGET_CLASS)]
-final class DeduplicateStamp implements StampInterface
+final class DeduplicateStamp implements NonSendableStampInterface
 {
     public function __construct(
         public readonly bool $enabled = true,
