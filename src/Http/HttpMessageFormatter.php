2c43882d42ff8c1bc87d0817d51d884c50c82e3a
Roland Franssen <1047696+ro0NL@users.noreply.github.com>
2024-11-27 13:59:41 +0100

    http-message-formatter

---
+++ src/Http/HttpMessageFormatter.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Etrias\PhpToolkit\Http;
+
+use Http\Message\Formatter;
+use Nyholm\Psr7\Request;
+use Psr\Http\Message\MessageInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+final class HttpMessageFormatter implements Formatter
+{
+    private const SENSITIVE_REQUEST_HEADERS = [
+        'authorization',
+        'proxy-authorization',
+        'www-authenticate',
+        'proxy-authenticate',
+        'x-amz-access-token',
+        'x-amz-security-token',
+        'x-auth-apikey',
+    ];
+    private const NON_SENSITIVE_RESPONSE_HEADERS = [
+        'content-type',
+        'retry-after',
+        'x-request-id',
+        'x-amzn-requestid',
+    ];
+
+    public function __construct(
+        #[Autowire(param: 'kernel.debug')]
+        private readonly bool $debug,
+    ) {}
+
+    public function formatRequest(RequestInterface $request): string
+    {
+        $message = \sprintf('%s %s HTTP/%s', $request->getMethod(), $request->getRequestTarget(), $request->getProtocolVersion());
+
+        foreach (array_diff_key($request->getHeaders(), array_flip(self::SENSITIVE_REQUEST_HEADERS)) as $name => $values) {
+            $message .= "\n".$name.': '.implode(', ', $values);
+        }
+
+        if ($this->debug && '' !== $body = self::getMessageBody($request)) {
+            $message .= "\n\n".$body;
+        }
+
+        return $message;
+    }
+
+    /**
+     * @deprecated
+     */
+    public function formatResponse(ResponseInterface $response): string
+    {
+        return $this->formatResponseForRequest($response, new Request('', ''));
+    }
+
+    public function formatResponseForRequest(ResponseInterface $response, RequestInterface $request): string
+    {
+        $message = \sprintf('HTTP/%s %s %s', $response->getProtocolVersion(), $response->getStatusCode(), $response->getReasonPhrase());
+        $debug = $this->debug || ($response->getStatusCode() >= 400);
+
+        if (!$debug) {
+            return $message;
+        }
+
+        foreach (array_intersect_key($response->getHeaders(), array_flip(self::NON_SENSITIVE_RESPONSE_HEADERS)) as $name => $values) {
+            $message .= "\n".$name.': '.implode(', ', $values);
+        }
+
+        if ('' !== $body = self::getMessageBody($response)) {
+            $message .= "\n\n".$body;
+        }
+
+        return $message;
+    }
+
+    private static function getMessageBody(MessageInterface $message): string
+    {
+        $contentType = $message->getHeader('content-type')[0] ?? null;
+
+        if (\in_array($contentType, ['application/octet-stream', 'application/pdf'], true)) {
+            return '***BINARY***';
+        }
+
+        $body = (string) $message->getBody();
+
+        if (str_starts_with($body, '%PDF-') && str_ends_with($body, '%%EOF')) {
+            return '***BINARY***';
+        }
+
+        return $body;
+    }
+}
