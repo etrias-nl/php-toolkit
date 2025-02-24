d6f4adc3e599be2ea336c260463fb9133868433d
Roland Franssen <ro0NL@users.noreply.github.com>
2025-02-24 14:43:35 +0100

    rector-v2

--- rector.php
+++ rector.php
@@ -6,21 +6,34 @@ use Rector\CodeQuality\Rector\ClassMethod\LocallyCalledStaticMethodToNonStaticRe
 use Rector\CodeQuality\Rector\Foreach_\UnusedForeachValueToArrayKeysRector;
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodingStyle\Rector\Assign\SplitDoubleAssignRector;
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\Config\RectorConfig;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
 use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withCache(__DIR__.'/var/rector-cache')
+    ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
-    ]);
-    $rectorConfig->skip([
+    ])
+    ->withRootFiles()
+    ->withImportNames(importShortClasses: false, removeUnusedImports: true)
+    ->withPhpSets()
+    ->withAttributesSets()
+    ->withComposerBased(twig: true, doctrine: true, phpunit: true, symfony: true)
+    ->withPreparedSets(
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+    )
+    ->withSkip([
         'tests/Fixtures/Messenger/envelope_compressed.php',
         'tests/Fixtures/Messenger/envelope_uncompressed.php',
         NullToStrictStringFuncCallArgRector::class,
@@ -31,29 +44,8 @@ return static function (RectorConfig $rectorConfig): void {
         AddOverrideAttributeToOverriddenMethodsRector::class,
         ReadOnlyClassRector::class,
         AddTypeToConstRector::class,
-    ]);
-    $rectorConfig->cacheDirectory(__DIR__.'/var/rector-cache');
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses(false);
-
-    // https://github.com/rectorphp/rector/blob/main/docs/rector_rules_overview.md
-    // https://github.com/rectorphp/rector-symfony/blob/main/docs/rector_rules_overview.md
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_83,
-        SetList::CODE_QUALITY,
-        SetList::EARLY_RETURN,
-        SetList::INSTANCEOF,
-        SetList::PRIVATIZATION,
-        SetList::TYPE_DECLARATION,
-        SymfonySetList::SYMFONY_60,
-        SymfonySetList::SYMFONY_61,
-        SymfonySetList::SYMFONY_62,
-        SymfonySetList::SYMFONY_63,
-        SymfonySetList::SYMFONY_64,
-        SymfonySetList::SYMFONY_70,
-        SymfonySetList::SYMFONY_71,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-    ]);
-};
+        NewlineAfterStatementRector::class,
+        CatchExceptionNameMatchingTypeRector::class,
+        SplitDoubleAssignRector::class,
+    ])
+;
