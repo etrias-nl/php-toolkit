58767ac981c888e85e461fd955e0f1a806e790ca
Roland Franssen :) <ro0NL@users.noreply.github.com>
2025-03-05 07:20:06 +0100

    Update DockerHandler.php

--- src/Monolog/DockerHandler.php
+++ src/Monolog/DockerHandler.php
@@ -17,6 +17,7 @@ final class DockerHandler extends AbstractProcessingHandler
     private const EXCLUDED_DEPRECATION_LOGS = [
         '~YAML mapping driver is deprecated and will be removed in Doctrine ORM~i',
         '~The "Symfony\\\Component\\\HttpKernel\\\DependencyInjection\\\Extension" class is considered internal~i',
+        '~Function utf8_encode\(\) is deprecated~i',
     ];
 
     private readonly string $command;
